a01010ea2fcfe926231a64532de1ca816701e8b6
Vineeth Reddy <vineethnaredla@gmail.com>
2021-11-17 11:29:33 -0600

    updated delete function in controllers

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -42,8 +42,16 @@ exports.vehicle_create_post = async function(req, res) {
 }; 
  
 // Handle vehcile delete form on DELETE. 
-exports.vehicle_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Vehcile delete DELETE ' + req.params.id); 
+exports.vehicle_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Vehicle.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    }
 }; 
  
 // Handle vehcile update form on PUT. 
