1e2e835bb83944675431104541ec51ea72ce4102
Vineeth Reddy <vineethnaredla@gmail.com>
2021-11-17 11:48:19 -0600

    Added vehicle delete

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -87,3 +87,17 @@ exports.vehicle_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+// Handle a show one view with id specified by query 
+exports.vehicle_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Vehicle.findById( req.query.id) 
+        res.render('vehicledetail',  
+{ title: 'Vehicle Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -5,4 +5,8 @@ const vehicle_controllers = require('../controllers/vehicle')
 /* GET home page. */
 router.get('/', vehicle_controllers.vehicle_view_all_Page);
 
+/* GET detail vehicle page */ 
+router.get('/detail', vehicle_controllers.vehicle_view_one_Page); 
+ 
+
 module.exports = router;
