9cc77faa8348f57c7b5a37d9ac778c791f739b9b
Vineeth Reddy <vineethnaredla@gmail.com>
2021-11-30 23:46:58 -0600

    Secured update

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 const vehicle_controllers = require('../controllers/vehicle')
 
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
+
 /* GET home page. */
 router.get('/', vehicle_controllers.vehicle_view_all_Page);
 
@@ -12,7 +22,7 @@ router.get('/detail', vehicle_controllers.vehicle_view_one_Page);
 router.get('/create', vehicle_controllers.vehicle_create_Page); 
 
 /* GET create update page */ 
-router.get('/update',vehicle_controllers.vehicle_update_Page);
+router.get('/update', secured, vehicle_controllers.vehicle_update_Page);
 
 /* GET create costume page */ 
 router.get('/delete', vehicle_controllers.vehicle_delete_Page); 
